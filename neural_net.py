e0ba36d03fa3e75b81d8a89e04adffcc89f4d26d
Rodrigo Hernández Mota <rohdzmota@gmail.com>
2016-10-24 19:06:08 -0500

    general framework

--- neural_net.py
+++ neural_net.py
@@ -4,7 +4,85 @@
 '''
 
 import numpy as np
-import pandas as pd
-import matplotlib.pyploy as plt
+#import pandas as pd
+#import matplotlib.pyplot as plt
 
-#class competitive_neurons:
+
+def vect2ind(x):
+    '''
+    Function to convert boolean vector to index. 
+    '''
+    for i in range(len(x)):
+        if x[i]:
+            return i
+
+# sigmoid function 
+sigmoid = lambda x: 1 / (1 + np.exp(-x))
+
+# linear function 
+linear = lambda w,x: np.dot(w, x)
+
+
+
+
+class adaline:
+    '''
+    
+    '''
+    desc = 'Adaline: simple neuron.'
+    
+    # basic information for the neuron
+    def __init__(self, phi = 'linear'):
+        self.phi = phi
+    
+
+
+
+class simple_logistic:
+    '''
+    
+    '''
+    desc = 'Simple logistic regression.'
+    
+    # basic information for the neuron
+    def __init__(self, phi = 'sigmoid'):
+        self.phi = phi
+
+
+
+class multiple_logistic: 
+    '''
+    
+    '''
+    desc = 'Multiple logistic regression.'
+
+
+
+
+
+class competitive_neurons:
+    '''
+    
+    '''
+    desc = 'Competitive Neural Network'
+    
+    # basic information for the neural net
+    def __init__(self, neurons = 2):
+        self.neurons = neurons
+   
+
+
+     
+class feed_forward_net:
+    '''
+    
+    '''
+    desc = 'Feed forwatd propagation neural net'
+    
+    # basic information for the neural net
+    def __init__(self, hidden_layers = 1, neurons = [1]):
+        self.hidden_layers = hidden_layers
+        self.neurons = neurons
+        
+    
+    
